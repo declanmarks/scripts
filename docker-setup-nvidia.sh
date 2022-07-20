c39c6662171b67f78591546acde4ef4b04f79157
Declan Marks <declan@marks.pw>
2022-07-20 21:40:55 +0100

    Update docker-setup-nvidia.sh

--- docker-setup-nvidia.sh
+++ docker-setup-nvidia.sh
@@ -2,6 +2,8 @@
 
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 curl -s -L https://nvidia.github.io/nvidia-docker/ubuntu22.04/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+sed -i 's/ubuntu18.04/ubuntu22.04/g' /etc/apt/sources.list.d/nvidia-docker.list
 apt update
 
 apt install -y nvidia-container-toolkit
